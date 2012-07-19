a6d19a7f74d74490a5d7a5c70d69933ee627b4be
Bruce Jackson <bruce.jackson@nasa.gov>
2012-07-19 14:39:36 -0400

    Tweaks to documentation

---
+++ DAVE2OTIS/src/gov/nasa/daveml/dave2otis/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * <p>Generates creation, setup and verification scripts to generate an
+ * OTIS-4&reg; model from a DAVE-ML file.</p>
+ *
+ * <p>
+ *    To use, run <br/>
+ *     <code>java -jar DAVE2OTIS.jar [-vcdelw] <em>inputFile</em></code>
+ * </p>
+ *
+ * @author Bruce Jackson, NASA Langley Research Center, bruce.jackson@nasa.gov
+ *
+ * @since 0.9.4
+ */
+package gov.nasa.daveml.dave2otis;
--- DAVE2POST/src/gov/nasa/daveml/dave2post/package-info.java
+++ DAVE2POST/src/gov/nasa/daveml/dave2post/package-info.java
@@ -1,14 +1,14 @@
 /**
  * <p>Generates creation, setup and verification scripts to generate a
- * Simulink&reg; model from a DAVE-ML file.</p>
+ * POST-II&reg; model from a DAVE-ML file.</p>
  *
  * <p>
  *    To use, run <br/>
- *     <code>java -jar DAVE2SL.jar [-vcdelw] <em>inputFile</em></code>
+ *     <code>java -jar DAVE2POST.jar [-vcdelw] <em>inputFile</em></code>
  * </p>
  *
  * @author Bruce Jackson, NASA Langley Research Center, bruce.jackson@nasa.gov
  *
- * @since 0.9
+ * @since 0.9.4
  */
-package gov.nasa.daveml.dave2sl;
+package gov.nasa.daveml.dave2post;
